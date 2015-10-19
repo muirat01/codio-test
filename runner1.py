c9faee7d1d98b34e837fb9b2315e3eb90e3063b1
muirat01 <muirat01@pudseygrangefield.co.uk>
2015-10-19 10:30:02 +0000

    First commit of runner1.py. This is a test.

---
+++ runner1.py
@@ -0,0 +1,10 @@
+__author__ = 'muirat01'
+#To change this template use Tools | Templates.
+
+Distance = 100
+SpeedOfRunner = 0
+MStoMPH = 2.24
+SpeedLimit = 20
+
+NameOfRunner = input("Name of Runner: \n")
+TimeTaken = float(input("Time Taken: "))
